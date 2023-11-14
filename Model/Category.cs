4030b450efffbf3745c867c2d20227aeff18689b
jmartinezcode <jmartinez.data@gmail.com>
2023-11-14 13:44:17 -0600

    [1.11] Annotations - customize the error message

--- Model/Category.cs
+++ Model/Category.cs
@@ -12,7 +12,7 @@ namespace NWConsole.Model
         }
 
         public int CategoryId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "YO - Enter the name!")]
         public string CategoryName { get; set; }
         public string Description { get; set; }
 
